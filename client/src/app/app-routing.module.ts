1fee37425c3807addbb0a0531fdbacccfe2ed07e
unknown <andromedia30@gmail.com>
2018-02-14 00:41:21 +0300

    router-setup

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'dashboard', component: DashboardComponent },
+
+
+    { path: '**', component: HomeComponent }
+]
+
+@NgModule({
+    declarations: [],
+    imports: [
+        RouterModule.forRoot(appRoutes)
+    ],
+    providers: [],
+    bootstrap: [],
+    exports: [
+        RouterModule
+    ]
+
+})
+
+export class AppRoutingModule { }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,32 @@
+////===== Angular Modules======
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
+//====My Modules====
+import { AppRoutingModule } from './app-routing.module';
 
+//=====My Components=====
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
